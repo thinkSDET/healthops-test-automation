f10482db0c239400ebaef26752f9bae0768076a3
Qa-Automation30 <qautomation32@gmail.com>
2026-08-14 13:26:11 +0530

    Auth-003

--- tests/modules/auth/login.spec.ts
+++ tests/modules/auth/login.spec.ts
@@ -3,15 +3,16 @@ import loginTestData from '../../../src/data/datasets/auth/login.json'
 
 test('AUTH-001 - Valid admin login redirects to dashboard',async({loginPage,dashboardHeaderComponent,page})=>{
     await page.goto("http://localhost:5173/login")
-    await loginPage.login(loginTestData.adminLogin.email,loginTestData.adminLogin.password)
+    await loginPage.login(loginTestData.admin.valid.email,loginTestData.admin.valid.password)
     await expect(page).toHaveURL('http://localhost:5173/dashboard')
     await expect(dashboardHeaderComponent.appUserName).toHaveText("System Administrator")
     await expect(dashboardHeaderComponent.appUserRole).toHaveText("ADMIN")
 })
 
 
-test('AUTH-003 - Valid wrong password shows error',async({loginPage,dashboardHeaderComponent,page})=>{
+test('AUTH-003 - Wrong password shows error',async({loginPage,page})=>{
     await page.goto("http://localhost:5173/login")
-    await loginPage.login(loginTestData.adminLoginWrongPassword.email,loginTestData.adminLoginWrongPassword.password)
+    await loginPage.login(loginTestData.admin.wrongPassword.email,loginTestData.admin.wrongPassword.password)
+    await expect(page).toHaveURL('http://localhost:5173/login')
     await expect(loginPage.authError).toHaveText("Invalid email or password")
 })
