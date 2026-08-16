1418ccaf7e74256a25131c32e2cd00e9f1f9061d
Qa-Automation30 <qautomation32@gmail.com>
2026-08-16 20:39:11 +0530

    change

--- tests/modules/patients/registerPatient.spec.ts
+++ tests/modules/patients/registerPatient.spec.ts
@@ -1,8 +1,8 @@
 import {test,expect} from '../../../src/fixtures/customFixtures'
 
-test('@AUTH-006 - Register patient with required profile fields',async({page,loginPage,registerPage,patientRegistrationTestData})=>{
+test('@AUTH-006 - Register patient with required profile fields',async({page,loginPage,registerPage,patientRegistration})=>{
      await page.goto("http://localhost:5173/login")
      await loginPage.openRegistrationForm()
-     await registerPage.register(patientRegistrationTestData.validPatientRegistration)
+     await registerPage.register(patientRegistration.validPatientRegistration)
      await page.waitForTimeout(20000)
 })
