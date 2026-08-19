f945d8a6c4ce21099d91d08de2fb99ffc32ae8e1
Qa-Automation30 <qautomation32@gmail.com>
2026-08-19 15:33:02 +0530

    Module sepration

--- tests/modules/auth/registerPharmacist.spec.ts
+++ tests/modules/auth/registerPharmacist.spec.ts
@@ -2,7 +2,7 @@
 import { Roles } from '../../../src/data/constants/roles'
 import { URLs } from '../../../src/data/constants/urls'
 import { test, expect } from '../../../src/fixtures/customFixtures'
-test("AUTH-008 - Register pharmacist  with required profile fields", async ({ page, loginPage, registerPage, validPharmacistRegistration, dashboardHeaderComponent }) => {
+test("AUTH-008 - Register pharmacist  with required profile fields", async ({ page, loginPage, registerPage, validPharmacistRegistration, dashboardPage }) => {
 
      await page.goto("http://localhost:5173/login")
      await loginPage.openRegistrationForm()
@@ -11,6 +11,6 @@ test("AUTH-008 - Register pharmacist  with required profile fields", async ({ pa
      await expect(page).toHaveURL(URLs.LOGIN)
      await loginPage.login(validPharmacistRegistration.emailAddress, validPharmacistRegistration.password)
      await expect(page).toHaveURL(URLs.DASHBOARD)
-     await expect(dashboardHeaderComponent.appUserName).toHaveText(`${validPharmacistRegistration.firstName} ${validPharmacistRegistration.lastName}`)
-     await expect(dashboardHeaderComponent.appUserRole).toHaveText(`${validPharmacistRegistration.accountType}`, {ignoreCase : true})
+     await expect(dashboardPage.dashBoardHeader.appUserName).toHaveText(`${validPharmacistRegistration.firstName} ${validPharmacistRegistration.lastName}`)
+     await expect(dashboardPage.dashBoardHeader.appUserRole).toHaveText(`${validPharmacistRegistration.accountType}`, {ignoreCase : true})
 })
