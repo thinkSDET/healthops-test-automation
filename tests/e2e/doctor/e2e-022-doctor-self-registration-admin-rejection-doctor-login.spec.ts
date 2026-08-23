c6d93d77d495c5952da7ea78885aa480a75922ad
Qa-Automation30 <qautomation32@gmail.com>
2026-08-23 20:24:19 +0530

    e2e-022

--- tests/e2e/doctor/e2e-022-doctor-self-registration-admin-rejection-doctor-login.spec.ts
+++ tests/e2e/doctor/e2e-022-doctor-self-registration-admin-rejection-doctor-login.spec.ts
@@ -2,31 +2,38 @@ import { Roles } from '../../../src/data/constants/roles';
 import { URLs } from '../../../src/data/constants/urls';
 import { expect, test } from '../../../src/fixtures/customFixtures'
 
-test('E2E-021: Doctor self-registers, gets approved by Admin, and logs in', async ({ page, loginPage, registerPage, validDoctorRegistration, dashboardPage, login, doctorRegistrationRequestPage }) => {
+test('E2E-022 - Doctor self-registration - Admin rejection - Doctor login with rejection notice', async ({ page, loginPage, registerPage, validDoctorRegistration, dashboardPage, login, doctorRegistrationRequestPage }) => {
+
     const doctorContact = `${validDoctorRegistration.emailAddress}`
-    const doctorFullName = `${validDoctorRegistration.firstName} ${validDoctorRegistration.lastName}`
     await test.step('Register as DOCTOR with valid unique details', async () => {
         await page.goto("http://localhost:5173/login")
         await loginPage.openRegistrationForm()
         await registerPage.register(validDoctorRegistration, Roles.DOCTOR)
         await expect(page).toHaveURL(URLs.LOGIN)
+
     });
 
     await test.step('Login as Admin and verify the new Doctor registration request is pending', async () => {
         await loginPage.login(login.admin.valid.email, login.admin.valid.password)
         await dashboardPage.dashBoardHeader.openDoctorRequests()
         await expect(doctorRegistrationRequestPage.getDoctorRegistrationStatus(doctorContact)).toHaveText('PENDING')
+
+    });
+    await test.step("Admin rejects the pending Doctor registration with a rejection reason", async () => {
+        await doctorRegistrationRequestPage.rejectDoctor(doctorContact, "Please updated details")
+        await expect(doctorRegistrationRequestPage.registrationRejectionMessage).toHaveText("Doctor registration rejected.")
+        await dashboardPage.dashBoardHeader.logout()
+
     });
 
-    await test.step('Admin Approve the Doctor registration request and verify the Doctor is ACTIVE', async () => {
-        expect(await doctorRegistrationRequestPage.getRegistrationActionMessageOnApprove(doctorContact)).toBe('Doctor registration approved successfully.')
-        await dashboardPage.dashBoardHeader.logout();
-    });
-    await test.step('Login as the approved Doctor', async () => {
+    await test.step("Login as rejected Doctor and verify login is allowed", async () => {
         await loginPage.login(validDoctorRegistration.emailAddress, validDoctorRegistration.password)
         await expect(page).toHaveURL(URLs.DASHBOARD)
-        await expect(dashboardPage.dashBoardHeader.appUserName).toHaveText(doctorFullName)
-        await expect(dashboardPage.dashBoardHeader.appUserRole).toHaveText("DOCTOR")
+
     });
 
+    await test.step("Verify Doctor dashboard opens with rejection popup and reason", async () => {
+        await expect(dashboardPage.updateAndSubmitButton).toBeVisible();
+
+    });
 })
